a5c4efd84425b77e5734fae94b6859ded4b9cc06
Ping <81280020+MyPingO@users.noreply.github.com>
2026-09-26 10:40:09 -0400

    Initialize shared templates in Global OnInit

--- examples/getting-started/MiliUIGlobal.lua
+++ examples/getting-started/MiliUIGlobal.lua
@@ -1,25 +1,25 @@
 -- Recommended shared MiliUI setup for projects with multiple interfaces.
--- Give this persistent client script the template-ID Script Parameters listed in
--- QUICK_START.md.
+-- Give this persistent client script the template-ID Script Variables listed in
+-- docs/Installation.md.
 
 local UI = require("MiliUI/init")
 
 local function InitTemplates()
     UI.InitTemplates({
         container = script:GetParam("Container Template ID"),
-        image = script:GetParam("Image Template ID"),
         text = script:GetParam("Text Template ID"),
-        button = script:GetParam("Button Template ID"),
-        cursorArea = script:GetParam("Cursor Area Template ID"),
+        textWindow = script:GetParam("Text Window Template ID"),
+        image = script:GetParam("Image Template ID"),
         animation = script:GetParam("UI Animation Template ID"),
         fullscreenAnimation = script:GetParam("Screen Animation Template ID"),
+        button = script:GetParam("Button Template ID"),
         keyHint = script:GetParam("Key Hint Template ID"),
-        textWindow = script:GetParam("Text Window Template ID"),
+        cursorArea = script:GetParam("Cursor Area Template ID"),
         gridScroller = script:GetParam("Grid Scroller Template ID"),
     })
 end
 
-function OnStart()
+function OnInit()
     InitTemplates()
 
     -- Optional project-wide polish can also live here. For example:
